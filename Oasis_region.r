fa114b6278a05fce9e34a250f9c96962a33089ef
ashikamani <34759802+ashikamani@users.noreply.github.com>
2023-04-07 12:55:30 -0400

    Add files via upload

---
+++ Oasis_region.r
@@ -0,0 +1,360 @@
+##########################################################################################
+## Project: Spatial OASIS 
+## File name: Oasis_region.r
+## Author: Ashika Mani
+## Date Created: 3/2/2023
+## Last Modified: 3/2/2023
+## Description:
+##
+##  This R program runs the Mimosa model on Spatial Oasis data
+##
+##########################################################################################
+
+##########################################################################################
+## Load the necessary libraries
+##########################################################################################
+
+library(fslr)
+library(neurobase)
+library(dplyr)
+library(oasis)
+library(oro.nifti)
+library(abind)
+library(ANTsR)
+library(extrantsr)
+library(WhiteStripe)
+library(pROC)
+library(grDevices)
+library(EveTemplate)
+library(lme4)
+.libPaths("/home/ashika/myRpackages")
+
+  
+##########################################################################################
+## Extract subject ids
+##########################################################################################
+in.dir <- '/project/Spatial_OASIS/processed/' 
+in.dir_T1_GS <- '/project/Spatial_OASIS/Images/'
+brainmask <-  '/project/Spatial_OASIS/OASIS_v1/'
+out.dir <- '/project/Spatial_OASIS/z_score/'
+intercept <- '/project/Spatial_OASIS/Intercept/'
+region <- '/project/Spatial_OASIS/Region/'
+subjectDir <- dir(in.dir)
+unique_subjects <- unique(substr(subjectDir, 1, 7))
+
+
+##########################################################################################
+## Randomly split the data into three sets for training, training, and testing
+## We have 98 subjects. Put 65 in each of training/training, and 33 in testing.
+##########################################################################################
+load(file=paste0(brainmask,"filepaths.rda"))
+for (i in seq(nrow(filepaths))) {
+  # Load files
+  visit_id = filepaths$visit_id[i]
+  fname = file.path(in.dir, 
+                    paste0(visit_id, '_labs_flip.nii.gz'))
+    filepaths$EVE[i] = fname
+  }
+
+x <- 1:2000
+seed = sample(x,1)
+#seed = 395
+set.seed(seed)
+sets <- split(unique_subjects, sample(rep(1:2,c(65,33))))
+
+training <- sets$'1'
+testing <- sets$'2'
+
+filepaths_train <- filepaths[filepaths$visit_id %in% training,] 
+filepaths_test <- filepaths[filepaths$visit_id %in% testing,] 
+
+##########################################################################################
+## First, we have to loop through the subjects in the training set and create
+## a training dataframe for each subject. 
+##########################################################################################
+
+for (i in 1:length(training)){
+  subject.id  <- training[i]
+  
+  print(paste("Beginning: ",subject.id))
+  
+  # Read in ith subject's images
+  T1_training = readnii(filepaths_train$T1[i])
+  T2_training = readnii(filepaths_train$T2[i])
+  FLAIR_training = readnii(filepaths_train$FLAIR[i])
+  PD_training = readnii(filepaths_train$PD[i])
+  gold_standard = readnii(filepaths_train$GS[i])
+  GS_flip <- neurobase::flip_img(gold_standard , x=TRUE, y = TRUE)
+  
+  # Create training dataframe for ith subject 
+  name <- paste("training_", subject.id, "_dataframe", sep="")
+  assign(name,oasis_train_dataframe(flair = FLAIR_training,t1 = T1_training,t2 = T2_training,pd = PD_training,gold_standard = GS_flip,brain_mask = readnii(filepaths$brainmask[i]),preproc=FALSE,normalize=TRUE,cores=4))
+  #save(list = name,file=paste0(region,"training_", subject.id, "_dataframe", ".rda"))
+
+  print(paste("Finished: ",subject.id))
+  
+}
+
+##########################################################################################
+## Loop through the trained dataframes for all subjects in the training dataset,
+## and add a column of the brain region labels to the trained dataframe
+##########################################################################################
+for (i in 1:length(training)){
+  subject.id  <- training[i]
+  
+  print(paste("Beginning: ",subject.id))
+  #load(file=paste0(region,"training_", subject.id, "_dataframe", ".rda"))
+  
+  # Retrieve ith subject's trained dataframe
+  trained_dataframe <- get(ls(pattern=paste("training_", subject.id, "_dataframe", sep="")))
+  
+  # Read in ith subject's brain labels
+  EVE = readNIfTI( paste0(in.dir, subject.id, '_labs_flip.nii.gz') , reorient=FALSE)
+ 
+  # Add column of brain labels to oasis dataframe for ith subject 
+  select <- as.vector(EVE)[as.vector(trained_dataframe$voxel_selection)>0]
+  trained_dataframe$oasis_dataframe <- cbind(trained_dataframe$oasis_dataframe,region = select)
+  
+  # Output new dataframe
+  out_name <- paste("training_", subject.id, "_dataframe", sep="")
+  assign(out_name,trained_dataframe)
+  #save(list = out_name,file=paste0(region,"training_", subject.id, "_dataframe", ".rda"))
+  
+  print(paste("Finished: ",subject.id))
+  
+}
+
+##########################################################################################
+## Train model for subjects in training set
+##########################################################################################
+# for (i in 1:length(training)) {
+#   subject.id  <- training[i]
+#   load(file= paste0("/project/Spatial_OASIS/Region/training_", subject.id, "_dataframe.rda"))
+# }
+#
+datlist <- mget(ls(pattern="training_"))
+# train_oasis_model_whole_brain = oasis_training(datlist[[1]][[1]],datlist[[2]][[1]],datlist[[3]][[1]],datlist[[4]][[1]],datlist[[5]][[1]],datlist[[6]][[1]],datlist[[7]][[1]],datlist[[8]][[1]],datlist[[9]][[1]],datlist[[10]][[1]],datlist[[11]][[1]], datlist[[12]][[1]],datlist[[13]][[1]],datlist[[14]][[1]],datlist[[15]][[1]],datlist[[16]][[1]],datlist[[17]][[1]],datlist[[18]][[1]],datlist[[19]][[1]],datlist[[20]][[1]],datlist[[21]][[1]],datlist[[22]][[1]],datlist[[23]][[1]],datlist[[24]][[1]],datlist[[25]][[1]],datlist[[26]][[1]],datlist[[27]][[1]],datlist[[28]][[1]],datlist[[29]][[1]],datlist[[30]][[1]],datlist[[31]][[1]], datlist[[32]][[1]],datlist[[33]][[1]],datlist[[34]][[1]],datlist[[35]][[1]],datlist[[36]][[1]],datlist[[37]][[1]],datlist[[38]][[1]],datlist[[39]][[1]],datlist[[40]][[1]],datlist[[41]][[1]],datlist[[42]][[1]],datlist[[43]][[1]],datlist[[44]][[1]],datlist[[45]][[1]],datlist[[46]][[1]],datlist[[47]][[1]],datlist[[48]][[1]],datlist[[49]][[1]],datlist[[50]][[1]],datlist[[51]][[1]], datlist[[52]][[1]],datlist[[53]][[1]],datlist[[54]][[1]],datlist[[55]][[1]],datlist[[56]][[1]],datlist[[57]][[1]],datlist[[58]][[1]],datlist[[59]][[1]],datlist[[60]][[1]],datlist[[61]][[1]],datlist[[62]][[1]],datlist[[63]][[1]],datlist[[64]][[1]],datlist[[65]][[1]])
+# save(file=paste0(brainmask,"train_oasis_model_whole_brain.rda"),train_oasis_model_whole_brain)
+
+
+##########################################################################################
+## Fit spatially-varying models
+##########################################################################################
+
+
+# Create list of dataframes
+list_of_train_dataframes <- list(datlist[[1]][[1]],datlist[[2]][[1]],datlist[[3]][[1]],datlist[[4]][[1]],datlist[[5]][[1]],datlist[[6]][[1]],datlist[[7]][[1]],datlist[[8]][[1]],datlist[[9]][[1]],datlist[[10]][[1]],datlist[[11]][[1]], datlist[[12]][[1]],datlist[[13]][[1]],datlist[[14]][[1]],datlist[[15]][[1]],datlist[[16]][[1]],datlist[[17]][[1]],datlist[[18]][[1]],datlist[[19]][[1]],datlist[[20]][[1]],datlist[[21]][[1]],datlist[[22]][[1]],datlist[[23]][[1]],datlist[[24]][[1]],datlist[[25]][[1]],datlist[[26]][[1]],datlist[[27]][[1]],datlist[[28]][[1]],datlist[[29]][[1]],datlist[[30]][[1]],datlist[[31]][[1]], datlist[[32]][[1]],datlist[[33]][[1]],datlist[[34]][[1]],datlist[[35]][[1]],datlist[[36]][[1]],datlist[[37]][[1]],datlist[[38]][[1]],datlist[[39]][[1]],datlist[[40]][[1]],datlist[[41]][[1]],datlist[[42]][[1]],datlist[[43]][[1]],datlist[[44]][[1]],datlist[[45]][[1]],datlist[[46]][[1]],datlist[[47]][[1]],datlist[[48]][[1]],datlist[[49]][[1]],datlist[[50]][[1]],datlist[[51]][[1]], datlist[[52]][[1]],datlist[[53]][[1]],datlist[[54]][[1]],datlist[[55]][[1]],datlist[[56]][[1]],datlist[[57]][[1]],datlist[[58]][[1]],datlist[[59]][[1]],datlist[[60]][[1]],datlist[[61]][[1]],datlist[[62]][[1]],datlist[[63]][[1]],datlist[[64]][[1]],datlist[[65]][[1]])
+
+# Convert to dataframe
+train_vectors_multi <- do.call(rbind, list_of_train_dataframes)
+train_vectors_multi <- as.data.frame(train_vectors_multi)
+train_vectors_multi <- train_vectors_multi[train_vectors_multi$GoldStandard<=1,]
+
+# Sort by region, and exclude 'background' (region=0)
+train_vectors_multi <- train_vectors_multi[order(train_vectors_multi$region),]
+train_vectors_multi <- train_vectors_multi[train_vectors_multi$region!=0,]
+
+# Fit the OASIS model separately for each brain region 
+formula1 = GoldStandard ~ FLAIR_10 *FLAIR + FLAIR_20*FLAIR + PD_10 *PD + PD_20 *PD + T2_10 *T2 + T2_20 *T2 + T1_10 *T1 + T1_20 *T1
+oasis_region <- by(train_vectors_multi, train_vectors_multi$region, function(x) glm(formula = formula1, data=x, family=binomial))
+##save(file=paste0(brainmask,"train_oasis_model_by_region.rda"),oasis_region)
+
+#load(file=paste0(region,"train_oasis_model_by_region.rda"))
+# Loop through oasis region and check for convergence errors. #
+# region_errors = c()
+# for (i in 1:dim(oasis_region)){
+#    region_errors[i] <- anyNA(as.vector(oasis_region[[i]]$coefficients))
+# }
+# names <- getEveMapLabels(type="II")$structure
+# names <- names[2:131] # Removing background
+# names <- names[sort(unique(train_vectors_multi$region))]
+# names[region_errors]
+
+# Fit the OASIS model with random intercepts on brain region
+# formula2 = GoldStandard ~ FLAIR_10 *FLAIR + FLAIR_20*FLAIR + PD_10 *PD + PD_20 *PD + T2_10 *T2 + T2_20 *T2 + T1_10 *T1 + T1_20 *T1 + (1|region)
+# oasis_glmer <- glmer(formula = formula2,data = train_vectors_multi,family = binomial,nAGQ=0,control=glmerControl(optimizer = "nloptwrap"))
+
+#save(file=paste0(intercept,"train_oasis_model_randomint.rda"),oasis_glmer)
+
+# Fit the OASIS model with random slopes on brain region
+# formula3 = GoldStandard ~ FLAIR_10 *FLAIR + FLAIR_20*FLAIR + PD_10 *PD + PD_20 *PD + T2_10 *T2 + T2_20 *T2 + T1_10 *T1 + T1_20 *T1 + (1 + FLAIR_10 *FLAIR + FLAIR_20*FLAIR + PD_10 *PD + PD_20 *PD + T2_10 *T2 + T2_20 *T2 + T1_10 *T1 + T1_20 *T1 |region)
+# oasis_glmer2 <- glmer(formula = formula3,data = train_vectors_multi,family = binomial)
+# save(file=paste0(brainmask,"train_oasis_model_random_slope.rda"),oasis_glmer2)
+
+
+
+##########################################################################################
+## Random intercepts OASIS predictions on testing dataset - by region
+##########################################################################################
+source('/project/Spatial_OASIS/predict_functions.R') # Load functions for GLM 
+for (i in 1:length(testing)){
+
+  subject.id  <- testing[i]
+  print(paste("Beginning: ",subject.id))
+  # Read in ith subject's images
+  brain_mask = readnii(filepaths_test$brainmask[i])
+  FLAIR = readnii(filepaths_test$FLAIR[i])
+  T1 = readnii(filepaths_test$T1[i])
+  T2 = readnii(filepaths_test$T2[i])
+  PD = readnii(filepaths_test$PD[i])
+  EVE = readnii(filepaths_test$EVE[i])
+
+    # Create training dataframe for ith subject 
+  out <- paste("oasis_regpred_", subject.id, sep="")
+  assign(out,oasis_predict_region(flair = FLAIR,t1 = T1,t2 = T2,pd = PD,brain_mask = brain_mask, EVE = EVE, preproc=FALSE,normalize=TRUE,binary=TRUE,cores=4))
+  #save(list = out, file=paste0(region,"oasis_regpred_", subject.id, ".rda"))
+  print(paste("Finished: ",subject.id))
+}
+#ls(pattern="pred_")
+
+# Add a column of brain labels to dataframe for each subject
+# for (i in 1:length(testing)){
+#   subject.id  <- testing[i]
+  
+#   print(paste("Beginning: ",subject.id))
+  
+#   # Retrieve ith subject's trained dataframe
+#   trained_dataframe <- get(ls(pattern=paste("pred_", subject.id, sep="")))
+  
+#   # Read in ith subject's brain labels
+#   EVE = readNIfTI( paste0(in.dir, subject.id, '_labs_flip.nii.gz') , reorient=FALSE)
+  
+#   # Add column of brain labels to oasis dataframe for ith subject 
+#   select <- as.vector(EVE)[as.vector(trained_dataframe$voxel_selection)>0]
+#   trained_dataframe$oasis_dataframe <- cbind(trained_dataframe$oasis_dataframe,region = select)
+  
+#   # Output new dataframe
+#   out_name <- paste("pred_", subject.id, "_dataframe", sep="")
+#   assign(out_name,trained_dataframe)
+#   #save(list = out_name, file=paste0(region,"pred_", subject.id, ".rda"))
+  
+#   print(paste("Finished: ",subject.id))
+  
+# }
+
+# # Now, for EACH subject we make separate predictions for EACH brain region.
+# for (i in 1:length(testing)){
+#   subject.id  <- testing[i]
+  
+#   print(paste("Beginning: ",subject.id))
+#   #load(file=paste0(region,"pred_", subject.id, ".rda"))
+#   # Retrieve ith subject's trained dataframe
+#   trained_dataframe <- get(ls(pattern=paste("pred_", subject.id, "_dataframe", sep="")))
+  
+#   # List of regions for this subject, exclude region 0 ('background')
+#   region_list <- sort(unique(trained_dataframe$oasis_dataframe$region))
+#   region_list <- region_list[region_list!=0]
+  
+#   # Add row number so we can resort to original order, then sort by region id
+#   trained_dataframe$oasis_dataframe$order <- 1:nrow(trained_dataframe$oasis_dataframe)
+#   trained_dataframe$oasis_dataframe <- trained_dataframe$oasis_dataframe[order(trained_dataframe$oasis_dataframe$region),]
+ 
+#   trained_dataframe$oasis_dataframe$rpred <- NA
+  
+#   # Loop through regions
+#   for (r in 1:length(region_list)){
+#     region.id  <- region_list[r]
+    
+#     # Make model predictions for region
+#     rdata  <- trained_dataframe$oasis_dataframe[trained_dataframe$oasis_dataframe$region==region.id,]
+#     rmodel <- oasis_region[[r]]
+#     rpred  <- predict( rmodel,
+#                        newdata = rdata,
+#                        type = 'response')
+#     trained_dataframe$oasis_dataframe$rpred[trained_dataframe$oasis_dataframe$region==region.id] <- rpred
+#   }
+#   # Sort back to original order
+#   trained_dataframe$oasis_dataframe <- trained_dataframe$oasis_dataframe[order(trained_dataframe$oasis_dataframe$order),]
+#   # Put the predictions onto the brain
+#   predictions_nifti <- niftiarr(trained_dataframe$brain_mask, 0)
+#   predictions_nifti[trained_dataframe$voxel_selection==1] <- trained_dataframe$oasis_dataframe$rpred
+#   predictions_nifti = datatyper(predictions_nifti, 
+#                                 datatype = convert.datatype()$FLOAT32,
+#                                 bitpix = convert.bitpix()$FLOAT32)
+
+#   # Smooth the probability map
+#   message("Smoothing Prediction")
+#   prob_map <- fslsmooth(predictions_nifti, sigma = 1.25, mask = trained_dataframe$brain_mask, retimg = TRUE, smooth_mask = TRUE, verbose = TRUE)
+#   writenii(prob_map, file = paste0('/project/Spatial_OASIS/Region/pm_region_', subject.id))
+#   # Threshold smoothed predictions
+#   binary_map <- prob_map
+#   binary_map[prob_map > 0.16] <- 1
+#   binary_map[prob_map <= 0.16] <- 0
+  
+#   # Output
+#   L = list(oasis_map = prob_map,
+#            binary_map = binary_map,
+#            unsmoothed_map = predictions_nifti,
+#            flair = trained_dataframe$flair,
+#            t1 = trained_dataframe$t1, t2 = trained_dataframe$t2,
+#            pd = trained_dataframe$pd,
+#            brain_mask = trained_dataframe$brain_mask,
+#            voxel_selection = trained_dataframe$voxel_selection)
+  
+#   out <- paste("val_", subject.id, "_df", sep="")
+#   assign(out,L)
+#   save(list = out, file=paste0(region,"oasis_byregion_pred_subject_", subject.id, ".rda"))
+  
+#   print(paste("Finished: ",subject.id))
+  
+# }
+
+##########################################################################################
+## Calculate AUC, partial AUC, and Dice similarity coefficient on regionwise predictions
+##########################################################################################
+
+# First, need to create a list of all the subject/region dataframes created above
+# for (i in 1:length(testing)) {
+#   subject.id  <- testing[i]
+#   load(file=paste0(region,"oasis_regpred_", subject.id, ".rda"))
+# }
+sub_reg_preds <- ls(pattern="oasis_regpred_")
+testing_region <- data.frame(subject=rep(NA,length(sub_reg_preds)),
+                                AUC=rep(NA,length(sub_reg_preds)),
+                                pAUC=rep(NA,length(sub_reg_preds)),
+                                Dice=rep(NA,length(sub_reg_preds)))
+
+
+for (i in 1:length(sub_reg_preds)){
+  
+  # Extract subject and region IDs from dataframe
+  subject.id = substr(sub_reg_preds[i], 15,25)
+  testing_region[i,"subject"] <- subject.id
+  
+  print(paste("Beginning: Subject ",subject.id))
+  
+  # Read in ith subject's lesion mask
+  gold_standard = readnii(filepaths_test$GS[i])
+  GS_flip <- neurobase::flip_img(gold_standard , x=TRUE, y = TRUE)
+
+ # Retrieve ith subject's predictions
+  L <- get(sub_reg_preds[i])
+  brain_mask = readnii(filepaths_test$brainmask[i])
+  prob_true_GS <- GS_flip[brain_mask==1]
+  probability_map <- L[["oasis_map"]]
+  #writenii(probability_map, file = paste0(region,"pm_region_" ,subject.id))
+  prob_true_predict <- probability_map[brain_mask==1]
+  ROC_obj = roc(as.vector(prob_true_GS), as.vector(prob_true_predict))
+  testing_region[i,"AUC"] <- auc(ROC_obj)[1]
+  testing_region[i,"pAUC"] <- auc(ROC_obj,partial.auc=c(1,0.99))[1]  
+  thresh = ifelse(prob_true_predict>.25,1,0)
+  dice_tab = as.data.frame(table(prob_true_GS,thresh))
+  TP = dice_tab[4,3]
+  FP = dice_tab[3,3]
+  FN = dice_tab[2,3]
+  DSC <- (2*TP)/((2*TP)+FP + FN)
+  testing_region[i,"Dice"] <- DSC
+  print(subject.id)
+  print(testing_region[i,"AUC"])
+  print(testing_region[i,"pAUC"])
+  print(DSC)
+}
+
+
+# Calculate AUC and partial AUC
+mean(testing_region$AUC)
+mean(testing_region$pAUC)/0.01
+mean(testing_region$Dice)
+df <- data.frame(seed,mean(testing_region$AUC),mean(testing_region$pAUC)/0.01,mean(testing_region$Dice))
+write.table(df, file = paste0(region,"results_region.csv"), sep = ',' , append = TRUE, quote = FALSE,col.names = FALSE, row.names = FALSE)
+
